63fb75e49885fa54eef9c5f774a983ad56cd621d
programagons <programagons@gmail.com>
2026-02-09 21:11:49 +0100

    Comentarios Base controller

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -24,45 +24,56 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 final class BaseController extends AbstractController {
 #PRUEBA
 
+    #Mostrar categorías
     #[Route('/categorias', name: 'categorias')]
     public function mostrar_categorias(ManagerRegistry $em): Response {
+        #Obtenemos las categorías de la base de datos
         $categorias = $em->getRepository(Categoria::class)->findAll();
+        #Renderizamos la vista 
         return $this->render('categorias/mostrar_categorias.html.twig', [
                     'categorias' => $categorias,
         ]);
     }
 
+    #Mostrar productos
     #[Route('/productos/{categoria}', name: 'productos')]
     public function mostrar_productos(ManagerRegistry $em, int $categoria): Response {
+        # Buscamos la categoría por el id
         $categoriaObjeto = $em->getRepository(Categoria::class)->find($categoria);
+        # Sacamos los productos de esa categoría
         $productos = $categoriaObjeto->getProductos();
+        # Renderizamos
         return $this->render('productos/mostrar_productos.html.twig', [
                     'productos' => $productos,
         ]);
     }
 
+    # WIP Mostrar detalles del producto
     #[Route('{producto}/detalles', name: 'detalles')]
     public function mostrar_detalles(): Response {
         
     }
 
+    # Añadir productos a la cesta
     #[Route('/anadir', name: 'anadir')]
     public function anadir_productos(EntityManagerInterface $em, Request $request, Cestacompra $cesta): Response {
-        //Recogemos los datos de entrada
+        #Recogemos los datos de entrada
         $productos_id = $request->request->all("productos_id");
         $unidades = $request->request->all("unidades");
-        //Vamos a obtener un array de objetos producto a través del id
+        #Vamos a obtener un array de objetos producto a través del id
         $productos = $em->getRepository(Producto::class)->findBy(['id' => $productos_id]);
-        //Cargamos los productos en la sesión
+        #Cargamos los productos en la sesión
         $cesta->cargar_productos($productos, $unidades);
         $valores_productos = array_values($productos);
 
-        //Sacamos la categoría del producto 
-        //$categoria_id = $valores_productos[0] -> getCategoria() -> getId();
+        #Sacamos la categoría del producto 
+        #$categoria_id = $valores_productos[0] -> getCategoria() -> getId();
 
+        #Redirigimos a la listsa de productos de la categoría del primer producto
         return $this->redirectToRoute('productos', ['categoria' => $valores_productos[0]->getCategoria()->getId()]);
     }
 
+    #Mostramos el contenido de la cesta
     #[Route('/cesta', name: 'cesta')]
     public function cesta(CestaCompra $cesta) {
         return $this->render('cesta/mostrar_cesta.html.twig', [
@@ -71,49 +82,53 @@ final class BaseController extends AbstractController {
         ]);
     }
 
+    # Eliminar o reducir producto
     #[Route('/eliminar', name: 'eliminar')]
     public function eliminar(Request $request, CestaCompra $cesta) {
-        // Recogemos los datos de entrada (los valores de la petición post)
+        # Recogemos los datos de entrada (los valores de la petición post)
         $producto_id = $request->request->get("producto_id");
         $unidades = $request->request->get("unidades");
+        # Eliminamos el producto de la cesta
         $cesta->eliminar_producto($producto_id, $unidades);
+        # Volvemos a la cesta 
         return $this->redirectToRoute('cesta');
     }
 
+    # Generamos un pedido con los datos de la cesta
     #[Route('/pedido', name: 'pedido')]
     public function pedido(EntityManagerInterface $em, CestaCompra $cesta, MailerInterface $mailer) {
 
-        // Variables
+        # Variables
         $error = 0;
-        // Obtenemos productos de la sesión
+        # Obtenemos productos de la sesión
         $productos = $cesta->get_Productos();
         $unidades = $cesta->get_Unidades();
 
-        // Si la cesta está vacía, como el codigo no puede ser null, nos daría error
+        # Si la cesta está vacía, como el codigo no puede ser null, nos daría error
         $pedido = $pedido ?? null;
 
-        // Flag para los errores
+        # Flag para los errores
         if (count($productos) == 0) {
-            // Error = 1 cuando NO hay productos en la cesta
+            # Error = 1 cuando NO hay productos en la cesta
             $error = 1;
         } else {
-            // Generamos un nuevo objeto pedido con sus setters
+            # Generamos un nuevo objeto pedido con sus setters
             $pedido = new Pedido();
 
-            // Setteamos el coste del pedido
+            # Setteamos el coste del pedido
             $pedido->setCoste($cesta->calcular_coste());
 
-            //Setteamos la fecha del pedido
+            #Setteamos la fecha del pedido
             $pedido->setFecha(new \Datetime());
 
-            // Setteamos el usuario
+            # Setteamos el usuario
             $pedido->setUsuario($this->getUser());
 
-            // Lo insertamos en la base de datos
+            # Lo insertamos en la base de datos
             $em->persist($pedido);
 
             foreach ($productos as $codigo_producto => $productoCesta) {
-                // Cargamos los pedidos en pedidoproducto
+                # Cargamos los pedidos en pedidoproducto
                 $pedidoProducto = new PedidoProducto();
                 $pedidoProducto->setPedido($pedido);
                 $producto = $em->getRepository(Producto::class)->findBy(['id' => $productoCesta->getId()])[0];
@@ -123,7 +138,7 @@ final class BaseController extends AbstractController {
                 $em->persist($pedidoProducto);
             }
             try {
-                //Guardamos todo con flush
+                #Guardamos todo con flush
                 $em->flush();
             } catch (Exception $ex) {
                 $error = 2;
@@ -131,22 +146,22 @@ final class BaseController extends AbstractController {
 
 
             if (!$error) {
-                // Obtenemos el id del usuario desde la sesión
+                # Obtenemos el id del usuario desde la sesión
                 $usuario_id = $this->getUser()->getId();
                 $usuario = $em->getRepository(Usuario::class)->find($usuario_id);
-                // Así sacamos el email del usuario
+                # Así sacamos el email del usuario
                 $destination_email = $usuario->getEmail();
 
-                // Mandamos el correo al email del usuario
+                # Mandamos el correo al email del usuario
 
                 $email = (new TemplatedEmail())
                         ->from(new Address('programagons@gmail.com', 'Tienda Symfony'))
                         ->to(new Address($destination_email))
                         ->subject('Confirmación de pedido #' . $pedido->getId())
-                        // indicamos la ruta de la plantilla
+                        # indicamos la ruta de la plantilla
                         ->htmlTemplate('correo.html.twig')
                         ->locale('es')
-                        // pasamos variables (clave => valor) a la plantilla
+                        # pasamos variables (clave => valor) a la plantilla
                         ->context([
                             'pedido_id' => $pedido->getId(),
                             'productos' => $cesta->get_Productos(),
@@ -165,16 +180,16 @@ final class BaseController extends AbstractController {
 
     #[Route('/historial', name: 'historial')]
     public function historial(EntityManagerInterface $em): Response {
-        // Obtenemos el usuario logueado
+        # Obtenemos el usuario logueado
         $usuario = $this->getUser();
 
-        // Buscamos los pedidos del usuario ordenados por fecha de forma descendente
+        # Buscamos los pedidos del usuario ordenados por fecha de forma descendente
         $pedidos = $em->getRepository(Pedido::class)->findBy(
                 ['Usuario' => $usuario],
                 ['fecha' => 'DESC']
         );
 
-        // Pasamos la información a la plantilla
+        # Pasamos la información a la plantilla
         return $this->render('pedido/historial.html.twig', [
                     'pedidos' => $pedidos
         ]);
